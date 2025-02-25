ad45469134c50f20dd0e0245fd7999c459ce35ae
Factories-git <official.factories@gmail.com>
2025-02-25 17:46:04 +0900

    Changes

--- 1로 만들기.py
+++ 1로 만들기.py
@@ -1,9 +1,6 @@
 n = int(input())
-arr = [0 for i in range(n+1)]
-try:
-    arr[2] = arr[3] = 1
-except IndexError:
-    pass
+arr = [0 for i in range(1000001)]
+arr[2] = arr[3] = 1
 for i in range(4,n+1):
     arr[i] = arr[i-1] +1
     if i % 2 == 0:
@@ -16,4 +13,3 @@ elif n == 2 or n == 3:
     print(1)
 else:
     print(arr[n])
-print(arr)
