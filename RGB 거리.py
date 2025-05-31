7e56d795514ecd202a1e5f95e4ebe83b6cb15fef
Factories-git <official.factories@gmail.com>
2025-05-31 21:15:50 +0900

    Changes

--- RGB 거리.py
+++ RGB 거리.py
@@ -1,9 +1,13 @@
 n = int(input())
-RGB = []
-dp = [''] * n
-for i in range(n):
-    RGB.append(list(map(int, input().split())))
-for i in range(n):
-    se = set(RGB[i])
-    m = min(RGB[i])
-    s = RGB[i].index(m)
+street = [list(map(int, input().split())) for i in range(n)]
+dp = [[0] * 3 for _ in range(n)]
+
+for i in range(3):
+    dp[0][i] = street[0][i]
+
+for i in range(1, n):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + street[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + street[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + street[i][2]
+
+print(min(dp[-1]))
